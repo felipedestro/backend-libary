fd8bad0dba2286bb26c0f9b54262db35177acb20
Felipe Destro <felipe.s.destro@gmail.com>
2024-04-19 22:54:21 -0400

    add: cors

--- dist/index.js
+++ dist/index.js
@@ -5,10 +5,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 require("dotenv/config");
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const routes_1 = require("./routes");
 const app = (0, express_1.default)();
 const PORT = process.env.PORT;
 app.use(express_1.default.json());
+app.use((0, cors_1.default)());
 app.use(routes_1.route);
 app.listen(PORT, () => console.log("listing port: " + PORT));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 import { route } from "./routes";
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use(route);
 
 app.listen(PORT, () => console.log("listing port: " + PORT));
